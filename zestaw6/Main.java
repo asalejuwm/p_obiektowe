d568b5196ea4e14677f575da9f0002f4e09fdfb6
asalejuwm <97965035+asalejuwm@users.noreply.github.com>
2024-11-26 12:33:30 +0100

    Update Main.java

--- zestaw6/Main.java
+++ zestaw6/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-    Produkt p1 = new Produkt("Pomidor",7.49,50);
+        Sklep sklep = new Sklep();
+        Produkt p1 = new Produkt("Pomidor",7.49,50);
     /*
     p1.wyswietlInformacje();
     p1.dodajDoMagazynu(5);
@@ -8,24 +9,49 @@ public class Main {
     p1.usunZMagazynu(10);
     p1.wyswietlInformacje();
      */
-    Produkt p2 = new Produkt("Woda niegazowana",1.99,1000);
-    //p2.wyswietlInformacje();
-    KoszykZakupowy koszyk1 = new KoszykZakupowy();
-    //k1.wyswietlProdukty();
-    koszyk1.dodajProdukt(p1,5);
-    koszyk1.dodajProdukt(p2,10);
-    //k1.wyswietlProdukty();
-    //System.out.println(k1.obliczCalkowitaWartosc());
-    Zamowienie z1 = new Zamowienie(koszyk1);
-    //z.wyswietlZamowienie();
-    //z.ustawStatusZamowienia();
-    //z.wyswietlZamowienie();
-    Klient klient1 = new Klient("Mariusz","Pudzian");
-    klient1.dodajZamowienie(z1);
-    klient1.wyswietlHistorieZamowien();
-    //z1.ustawStatusZamowienia();
-    //klient1.wyswietlHistorieZamowien();
+        Produkt p2 = new Produkt("Woda niegazowana",1.99,1000);
+        //p2.wyswietlInformacje();
+        sklep.dodaj(p1);
+        sklep.dodaj(p2);
 
+        KoszykZakupowy koszyk1 = new KoszykZakupowy();
+        KoszykZakupowy koszyk2 = new KoszykZakupowy();
+        //k1.wyswietlProdukty();
+        koszyk1.dodajProdukt(p1,5);
+        koszyk1.dodajProdukt(p2,10);
+        koszyk2.dodajProdukt(p2,13);
+        //k1.wyswietlProdukty();
+        //System.out.println(k1.obliczCalkowitaWartosc());
+        Platnosc platnosc1 = new Platnosc();
+        Zamowienie z1 = new Zamowienie(koszyk1,new Platnosc());
+        Zamowienie z2 = new Zamowienie(koszyk2, new Platnosc());
+        platnosc1 = new Platnosc(z2,"Nieoplacone");
+        //z.wyswietlZamowienie();
+        //z.ustawStatusZamowienia();
+        //z.wyswietlZamowienie();
+        Klient klient1 = new Klient("Mariusz","Pudzian");
+        klient1.dodajZamowienie(z1);
+        klient1.dodajZamowienie(z2);
+        klient1.wyswietlHistorieZamowien();
+        //z1.ustawStatusZamowienia();
+        //klient1.wyswietlHistorieZamowien();
 
+        //System.out.println(koszyk1.obliczCalkowitaWartosc());
+        //System.out.println(koszyk2.obliczCalkowitaWartosc());
+        System.out.println(klient1.obliczLacznyKosztZamowien());
+
+        //sklep.dodajProdukt();
+        //sklep.wyswietlOferty();
+
+        sklep.zakupy(p1,2,z2);
+        sklep.zakupy(p2,3,z2); //funkcja nie doda p2, bo ten produkt jest juz w zamowieniu
+        z2.wyswietlZamowienie();
+        z2.platnosc = platnosc1;
+        System.out.println(platnosc1.statusPlatnosci);
+        platnosc1.zaplac();
+        System.out.println(platnosc1.statusPlatnosci);
+        System.out.println(z2.platnosc==platnosc1);
+        z2.finalizujZamowienie();
+        System.out.println(z2.statusZamowienia);
     }
 }
