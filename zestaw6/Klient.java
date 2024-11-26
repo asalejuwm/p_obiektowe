2a51a765001cfbc9cdeb2bc4f4be1dd34b7649ea
asalejuwm <97965035+asalejuwm@users.noreply.github.com>
2024-11-26 12:34:08 +0100

    Update Klient.java

--- zestaw6/Klient.java
+++ zestaw6/Klient.java
@@ -3,27 +3,29 @@ public class Klient {
     public String imie;
     public String nazwisko;
     public ArrayList<Zamowienie> listaZamowien;
-    public Klient(String imie, String nazwisko){
+
+    public Klient(String imie, String nazwisko) {
         this.imie = imie;
         this.nazwisko = nazwisko;
         this.listaZamowien = new ArrayList<>();
     }
 
-    public void dodajZamowienie(Zamowienie z){
+    public void dodajZamowienie(Zamowienie z) {
         listaZamowien.add(z);
     }
 
-    public void wyswietlHistorieZamowien(){
-        System.out.println("Lista zamowien dla klienta "+imie+" "+nazwisko+": ");
-        for(Zamowienie z : listaZamowien){
+    public void wyswietlHistorieZamowien() {
+        System.out.println("Lista zamowien dla klienta " + imie + " " + nazwisko + ": ");
+        for (Zamowienie z : listaZamowien) {
             z.wyswietlZamowienie();
         }
     }
-    public int obliczLacznyKosztZamowien(){
+    public double obliczLacznyKosztZamowien(){
+        double res = 0;
         for(Zamowienie z : listaZamowien){
-            for(KoszykZakupowy k : z){
-
-            }
+            res+=z.koszykZakupowy.obliczCalkowitaWartosc();
         }
+        return res;
     }
+
 }
