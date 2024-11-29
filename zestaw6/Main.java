5d380bd837def9f9eb04fc08fccf8739933954fd
asalejuwm <97965035+asalejuwm@users.noreply.github.com>
2024-11-29 17:47:44 +0100

    Update Main.java

--- zestaw6/Main.java
+++ zestaw6/Main.java
@@ -1,3 +1,4 @@
+
 public class Main {
     public static void main(String[] args) {
         Sklep sklep = new Sklep();
@@ -11,9 +12,8 @@ public class Main {
      */
         Produkt p2 = new Produkt("Woda niegazowana",1.99,1000);
         //p2.wyswietlInformacje();
-        sklep.dodaj(p1);
-        sklep.dodaj(p2);
-
+        sklep.dodajProdukt(p1);
+        sklep.dodajProdukt(p2);
         KoszykZakupowy koszyk1 = new KoszykZakupowy();
         KoszykZakupowy koszyk2 = new KoszykZakupowy();
         //k1.wyswietlProdukty();
@@ -32,20 +32,20 @@ public class Main {
         Klient klient1 = new Klient("Mariusz","Pudzian");
         klient1.dodajZamowienie(z1);
         klient1.dodajZamowienie(z2);
-        klient1.wyswietlHistorieZamowien();
+        //klient1.wyswietlHistorieZamowien();
         //z1.ustawStatusZamowienia();
         //klient1.wyswietlHistorieZamowien();
 
         //System.out.println(koszyk1.obliczCalkowitaWartosc());
         //System.out.println(koszyk2.obliczCalkowitaWartosc());
-        System.out.println(klient1.obliczLacznyKosztZamowien());
+        //System.out.println(klient1.obliczLacznyKosztZamowien());
 
-        //sklep.dodajProdukt();
-        //sklep.wyswietlOferty();
+        sklep.wyswietlOferty();
 
         sklep.zakupy(p1,2,z2);
         sklep.zakupy(p2,3,z2); //funkcja nie doda p2, bo ten produkt jest juz w zamowieniu
         z2.wyswietlZamowienie();
+        /*
         z2.platnosc = platnosc1;
         System.out.println(platnosc1.statusPlatnosci);
         platnosc1.zaplac();
@@ -53,5 +53,21 @@ public class Main {
         System.out.println(z2.platnosc==platnosc1);
         z2.finalizujZamowienie();
         System.out.println(z2.statusZamowienia);
+         */
+
+        Magazyn magazyn = new Magazyn();
+        magazyn.dodajProdukt(p1);
+        magazyn.dodajProdukt(p2);
+        //magazyn.wyswietlAsortyment();
+
+        Adres adres1 = new Adres("Pudziana",5,8,"Tychy","99-999");
+        adres1.pokaz();
+
+        System.out.println("12-345".compareTo("12-344"));
+
+        //throw new illegalargumentexception
+        //@Override
+        //to string
+
     }
 }
